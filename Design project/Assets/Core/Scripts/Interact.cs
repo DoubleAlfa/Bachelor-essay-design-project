b1e1295a617adfb3df747292f8f8cfe1f05a23d5
Satkaaktas <Timmy_nitr0@hotmail.com>
2018-12-03 16:24:05 +0100

    Interact
    Interacttest finns i Testis-scenen

---
+++ Design project/Assets/Core/Scripts/Interact.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    void Interact();
+}
+
+public class Interact : MonoBehaviour
+{
+    RaycastHit hit;
+    IInteractable interactObject;
+    string objectName = "";
+    bool interact;
+    GameObject interactIcon;
+    Transform Camera;
+    // Use this for initialization
+    void Start()
+    {
+        interactIcon = GameObject.FindGameObjectWithTag("Canvas").transform.GetChild(0).gameObject;
+        Camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Physics.Raycast(Camera.position, Camera.forward, out hit, int.MaxValue))
+        {
+            
+            if (hit.transform.tag == "Interactable" && objectName != hit.transform.name)
+            {
+                InteractActive(true);
+            }
+            else if(hit.transform.tag != "Interactable")
+            {
+                InteractActive(false);
+            }
+        }
+        if(interact && Input.GetKeyDown(KeyCode.E))
+        {
+            interactObject.Interact();
+        }
+
+    }
+
+    void InteractActive(bool active)
+    {
+        interact = active;
+        if (active)
+        {
+            interactObject = hit.transform.GetComponent<IInteractable>();
+            objectName = hit.transform.name;
+        }
+        else
+        {
+            objectName = "";
+        }
+        interactIcon.SetActive(active);
+    }
+}
---
+++ Design project/Assets/Prototyping/InteractTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractTest : MonoBehaviour, IInteractable
+{
+    float counter;
+    bool spin;
+
+    void Update()
+    {
+        if(spin)
+        {
+            print("EYEYEYE");
+            transform.Rotate(Vector3.up * Time.deltaTime * 50);
+            counter += Time.deltaTime * 50;
+            if(counter >= 90)
+            {
+                spin = false;
+                transform.Rotate(Vector3.down * (counter - 90));
+                counter = 0;
+            }
+        }
+    }
+	public void Interact()
+    {
+        print("JAHAAA!");
+        if(!spin)
+        {
+            spin = true;
+        }
+    }
+
+
+}
