d1a21ccb72bbc4cec623e23a1acf08e17d9e9c88
Satkaaktas <Timmy_nitr0@hotmail.com>
2018-12-14 11:09:29 +0100

    First dialogue

---
+++ Design project/Assets/Core/Scripts/Hint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hint : MonoBehaviour {
+
+
+    AudioSource speech;
+    [SerializeField]
+    AudioClip[] hints;
+    AudioClip activeHint;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        speech = gameObject.AddComponent<AudioSource>();
+
+        activeHint = hints[0];
+        speech.clip = activeHint;
+	}
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            speech.clip = activeHint;
+            speech.Play();
+        }
+    }
+
+
+}
